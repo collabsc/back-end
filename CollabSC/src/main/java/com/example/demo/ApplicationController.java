238b948b2709442117098fe015599272c40790e8
Nolan Jimenez <nolan1299@gmail.com>
2019-04-07 22:29:29 -0700

    working post/get/delete for applications

--- CollabSC/src/main/java/com/example/demo/ApplicationController.java
+++ CollabSC/src/main/java/com/example/demo/ApplicationController.java
@@ -10,10 +10,11 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.server.ResponseStatusException;
 
+@RestController
 public class ApplicationController {
-	
 	private final ApplicationRestRepository repository;
 	
 	ApplicationController(ApplicationRestRepository repository){
@@ -25,40 +26,25 @@ public class ApplicationController {
 		Iterable<Application> it = repository.findAll();
 		List<Application> applications = new ArrayList<Application>();
 		it.forEach(applications::add);
-		System.out.println(applications.size());
-		return applications;		
+		return applications;
 	}
 	
-	@GetMapping("/applications/{userid}")
-	List<Application> getApplication(@PathVariable("userid") Integer userid) {
-//		List<Application> applications = all();
-//		System.out.println("run");
-//		for (Application app : applications) {
-//			if (app.getUserid() == userid)
-//				System.out.println(app.getUserid());
-//		}
-//		if (!applications.isEmpty())
-//			return applications;
-//		else
-//			throw new ResponseStatusException(HttpStatus.NOT_FOUND , "applicaiton with "
-//					+ userid + " do not exist");
-			
-		if (!repository.findApplicationByUserId(userid).isEmpty())
-			return repository.findApplicationByUserId(userid);
-		else
-			throw new ResponseStatusException(HttpStatus.NOT_FOUND , "applications with id " + 
-					userid + " do not exist");
-	}
-	
-	@RequestMapping (value = "/applications" , method=RequestMethod.POST)
-	Application newApplication (@RequestBody Application newApplication) {
+	@RequestMapping(value = "/applications" , method=RequestMethod.POST)
+	Application newApplication(@RequestBody Application newApplication) {
 		return repository.save(newApplication);
 	}
 	
-	
-	@DeleteMapping(value = "/applications")
-	void deleteApplication(@PathVariable Long applicationid) {
-		repository.deleteById(applicationid);
+	@GetMapping(value = "/applications/userid/{userid}")
+	List<Application> findApplicationByUserId(@PathVariable Integer userid){
+		if (!repository.findApplicationByUserId(userid).isEmpty())
+			return repository.findApplicationByUserId(userid);
+		else
+			throw new ResponseStatusException(HttpStatus.NOT_FOUND , "application with userid "
+					+ userid + " does not exist");
 	}
-
+	
+	@DeleteMapping (value = "/applications/appid")
+	void delteApplication(Long userid) {
+		repository.deleteById(userid);
+	}
 }
--- CollabSC/src/main/java/com/example/demo/ApplicationRestRepository.java
+++ CollabSC/src/main/java/com/example/demo/ApplicationRestRepository.java
@@ -5,8 +5,10 @@ import java.util.List;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.repository.query.Param;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
+@RepositoryRestResource
 public interface ApplicationRestRepository extends CrudRepository<Application , Long> {
-	@Query("FROM Application a WHERE a.userid = :userid")
-	List<Application> findApplicationByUserId(@Param("userid") Integer id);
+	@Query ("FROM Application a WHERE a.userid = :userid")
+	List<Application> findApplicationByUserId(@Param("userid") Integer userid);
 }
